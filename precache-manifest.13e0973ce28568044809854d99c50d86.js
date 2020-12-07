6ae9cbf0d073eb802e1a7df0d26aa07c9e182207
IlyaErshovDev <yosh7595@gmail.com>
2020-12-07 17:05:59 +0300

    Updates

---
+++ precache-manifest.13e0973ce28568044809854d99c50d86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b046716437f4f930e64dee2f939a79",
+    "url": "/simple-restaurant-app/index.html"
+  },
+  {
+    "revision": "0fa729bb5cd34285d88a",
+    "url": "/simple-restaurant-app/static/css/main.69d9c981.chunk.css"
+  },
+  {
+    "revision": "a3108ae2ab99500ba4aa",
+    "url": "/simple-restaurant-app/static/js/2.11692327.chunk.js"
+  },
+  {
+    "revision": "0fa729bb5cd34285d88a",
+    "url": "/simple-restaurant-app/static/js/main.c6484326.chunk.js"
+  },
+  {
+    "revision": "219363d28031026e8d7f",
+    "url": "/simple-restaurant-app/static/js/runtime~main.0d9bb980.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/simple-restaurant-app/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/simple-restaurant-app/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
